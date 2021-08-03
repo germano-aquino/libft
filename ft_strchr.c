a53746ff64b6470728d8cfd70ab8cca0fbd82376
Germano Aquino <germano.r.aquino@gmail.com>
2021-08-03 02:14:17 -0300

    strchr

---
+++ ft_strchr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+char    *ft_strchr(const char *str, int c)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != c && str[i] != '\0')
+        i++;
+    if (str[i] != '\0' || c == '\0')
+        return &(str[i]);
+    else
+        return (0);
+}
