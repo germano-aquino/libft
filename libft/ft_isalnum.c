bbdcb8cf0e529f3f7f2af3cbb77283d99c70a05c
Germano Aquino <germano.r.aquino@gmail.com>
2021-08-20 19:12:10 -0300

    isalnum correction

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -1,13 +1,6 @@
 int	ft_isalnum(int c)
 {
-	unsigned char	ch;
-
-	ch = (unsigned char) c;
-	if ('a' <= ch && ch <= 'z')
-		return (1);
-	if ('A' <= ch && ch <= 'Z')
-		return (1);
-	if ('0' <= ch && ch <= '9')
+	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
 	return (0);
 }
