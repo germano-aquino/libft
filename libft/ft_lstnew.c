b085b7a350a208c1227e1269b90edcdbfd121916
Germano Aquino <germano.r.aquino@gmail.com>
2021-08-21 11:20:34 -0300

    Bonus approved in unit

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -5,14 +5,10 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list *new;
 
-	new = NULL;
-	if (content)
-	{
-		new = (t_list *)malloc(sizeof(t_list));
-		if (new == NULL)
-			return (0);
-		(*new).content = content;
-		(*new).next = NULL;
-	}
+	new = (t_list *)malloc(sizeof(t_list));
+	if (new == NULL)
+		return (0);
+	(*new).content = content;
+	(*new).next = NULL;
 	return (new);
 }
